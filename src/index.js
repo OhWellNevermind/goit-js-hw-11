79d9b863734b70468eb8bb90041fc6d591fa69dd
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-07-21 03:27:46 +0300

    styles added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import InfiniteScroll from 'infinite-scroll';
+
 const API_KEY = '38376053-36f58a073519ff95a963b2d32';
+const BASE_URL = 'https://pixabay.com/api/';
 
 const elements = {
   form: document.querySelector('.search-form'),
@@ -10,11 +13,22 @@ const elements = {
   input: document.querySelector('.form-input'),
 };
 
-elements.form.addEventListener('submit', event => {
+elements.form.addEventListener('submit', async event => {
   event.preventDefault();
   const userQuery = elements.input.value;
-  const url = `https://pixabay.com/api/?key=38376053-36f58a073519ff95a963b2d32&q=${userQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const images = fetchImages(url)
+  const params = {
+    key: API_KEY,
+    q: userQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 40,
+  };
+  const paramsString = new URLSearchParams(params).toString();
+  console.log(paramsString);
+  const url = `${BASE_URL}?${paramsString}`;
+  await fetchImages(url)
     .then(data => {
       return data.map(image => {
         return {
@@ -29,39 +43,16 @@ elements.form.addEventListener('submit', event => {
       });
     })
     .then(images => {
-      const html = images
-        .map(
-          ({
-            webformatURL,
-            largeImageURL,
-            tags,
-            likes,
-            views,
-            comments,
-            downloads,
-          }) => {
-            return `
-            <div class="photo-card">
-            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-                <p class="info-item">
-                <b>Likes: </b>${likes}
-                </p>
-                <p class="info-item">
-                <b>Views: </b>${views}
-                </p>
-                <p class="info-item">
-                <b>Comments: </b>${comments}
-                </p>
-                <p class="info-item">
-                <b>Downloads: </b>${downloads}
-                </p>
-            </div>
-            </div>`;
-          }
-        )
-        .join();
+      const html = createMarkUp(images);
+
+      Notiflix.Notify.success(`Here is your images! Find the best one;)`);
+      elements.gallery.innerHTML = '';
       elements.gallery.insertAdjacentHTML('beforeend', html);
+      const lightbox = new SimpleLightbox('.gallery a');
+      let infScroll = new InfiniteScroll('.container', {
+        path: '`https://pixabay.com/api/?key=38376053-36f58a073519ff95a963b2d32&q=${userQuery}&image_type=photo&orientation=horizontal&safesearch=true`',
+      });
+      infScroll.loadNextPage();
     });
 });
 
@@ -78,4 +69,41 @@ async function fetchImages(url) {
   } catch (error) {
     Notiflix.Notify.failure(error.message);
   }
+}
+
+function createMarkUp(array) {
+  return array
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+            <li class="photo-card">
+              <a class="gallery__link" href="${largeImageURL}">
+                  <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+              </a>
+              <div class="info">
+                  <p class="info-item">
+                  <b>Likes: </b>${likes}
+                  </p>
+                  <p class="info-item">
+                  <b>Views: </b>${views}
+                  </p>
+                  <p class="info-item">
+                  <b>Comments: </b>${comments}
+                  </p>
+                  <p class="info-item">
+                  <b>Downloads: </b>${downloads}
+                  </p>
+              </div>
+            </li>`;
+      }
+    )
+    .join('');
 }
