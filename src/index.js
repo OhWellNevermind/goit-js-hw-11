71c486c28fc0f6eb31c0febba2d267765fa55c96
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-07-24 15:22:02 +0300

    woring commit

--- src/index.js
+++ src/index.js
@@ -6,29 +6,75 @@ import InfiniteScroll from 'infinite-scroll';
 
 const API_KEY = '38376053-36f58a073519ff95a963b2d32';
 const BASE_URL = 'https://pixabay.com/api/';
+const IMG_COUNT = 40;
+let page = 0;
 
 const elements = {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   input: document.querySelector('.form-input'),
+  loadBtn: document.querySelector('.load-more'),
 };
 
-elements.form.addEventListener('submit', async event => {
-  event.preventDefault();
+elements.form.addEventListener('submit', hadleSubmit);
+elements.loadBtn.addEventListener('click', handleLoadClick);
+let lightbox = new SimpleLightbox('.gallery a');
+function handleLoadClick() {
   const userQuery = elements.input.value;
-  const params = {
+  const url = generatePixabayURL(userQuery);
+  console.log(url);
+  page += 1;
+  fetchImages(url)
+    .then(data => {
+      return data.map(image => {
+        return {
+          webformatURL: image.webformatURL,
+          largeImageURL: image.largeImageURL,
+          tags: image.tags,
+          likes: image.likes,
+          views: image.views,
+          comments: image.comments,
+          downloads: image.downloads,
+        };
+      });
+    })
+    .then(images => {
+      const html = createMarkUp(images);
+
+      Notiflix.Notify.success(`Here is your images! Find the best one;)`);
+      elements.gallery.insertAdjacentHTML('beforeend', html);
+
+      lightbox = lightbox.refresh();
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+    });
+}
+
+function generatePixabayURL(query) {
+  const params = new URLSearchParams({
     key: API_KEY,
-    q: userQuery,
+    q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    page: 1,
-    per_page: 40,
-  };
-  const paramsString = new URLSearchParams(params).toString();
-  console.log(paramsString);
-  const url = `${BASE_URL}?${paramsString}`;
-  await fetchImages(url)
+    per_page: IMG_COUNT,
+    page,
+  });
+  return `${BASE_URL}?${params}`;
+}
+
+function hadleSubmit(event) {
+  event.preventDefault();
+  page = 1;
+  const userQuery = elements.input.value;
+  const url = generatePixabayURL(userQuery);
+  fetchImages(url)
     .then(data => {
       return data.map(image => {
         return {
@@ -48,13 +94,12 @@ elements.form.addEventListener('submit', async event => {
       Notiflix.Notify.success(`Here is your images! Find the best one;)`);
       elements.gallery.innerHTML = '';
       elements.gallery.insertAdjacentHTML('beforeend', html);
-      const lightbox = new SimpleLightbox('.gallery a');
-      let infScroll = new InfiniteScroll('.container', {
-        path: '`https://pixabay.com/api/?key=38376053-36f58a073519ff95a963b2d32&q=${userQuery}&image_type=photo&orientation=horizontal&safesearch=true`',
-      });
-      infScroll.loadNextPage();
+
+      lightbox = lightbox.refresh();
     });
-});
+  page = 2;
+  elements.loadBtn.classList.remove('hidden');
+}
 
 async function fetchImages(url) {
   try {
