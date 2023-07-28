83909fbcac9717a3ab03ea35f5626f43bb2a4f9c
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-07-28 19:47:26 +0300

    generate url function moved to api.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,18 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { fetchImages } from './js/api';
+import { fetchImages, generatePixabayURL, IMG_COUNT } from './js/api';
 import { createMarkUp } from './js/markup';
 import { elements } from './js/htmlElements';
 
-const API_KEY = '38376053-36f58a073519ff95a963b2d32';
-const BASE_URL = 'https://pixabay.com/api/';
-const IMG_COUNT = 40;
 let page = 0;
 elements.form.addEventListener('submit', hadleSubmit);
 elements.loadBtn.addEventListener('click', handleLoadClick);
 
 function handleLoadClick() {
   const userQuery = elements.input.value;
-  const url = generatePixabayURL(userQuery);
   page += 1;
+  const url = generatePixabayURL(userQuery, page);
   fetchImages(url)
     .then(({ data, totalHits }) => {
       if (Math.ceil(page > totalHits / IMG_COUNT)) {
@@ -50,19 +47,6 @@ function handleLoadClick() {
     });
 }
 
-function generatePixabayURL(query) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: IMG_COUNT,
-    page,
-  });
-  return `${BASE_URL}?${params}`;
-}
-
 function hadleSubmit(event) {
   event.preventDefault();
   page = 1;
@@ -72,7 +56,7 @@ function hadleSubmit(event) {
     elements.gallery.innerHTML = '';
     return;
   }
-  const url = generatePixabayURL(userQuery);
+  const url = generatePixabayURL(userQuery, page);
   fetchImages(url)
     .then(({ data, totalHits }) => {
       if (Math.ceil(page < totalHits / IMG_COUNT)) {
--- src/js/api.js
+++ src/js/api.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import { elements } from './htmlElements';
 
+const API_KEY = '38376053-36f58a073519ff95a963b2d32';
+const BASE_URL = 'https://pixabay.com/api/';
+const IMG_COUNT = 40;
+
 async function fetchImages(url) {
   try {
     const response = axios.get(url);
@@ -21,4 +25,17 @@ async function fetchImages(url) {
   }
 }
 
-export { fetchImages };
+function generatePixabayURL(query, page) {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: IMG_COUNT,
+    page,
+  });
+  return `${BASE_URL}?${params}`;
+}
+
+export { fetchImages, generatePixabayURL, IMG_COUNT };
